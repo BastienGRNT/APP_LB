23b48941124292ab0d207b4e3dbd3be86ae8ed87
lducrocq <lducrocq@n-ine.fr>
2024-12-10 15:32:15 +0100

    table sql + front pour l'ajout d'ami ( à faire : l'API)

---
+++ BET APP/API/API/Data/Friendships.cs
@@ -0,0 +1,11 @@
+﻿namespace API.Data;
+
+public class Friendships
+{
+    public int Id { get; set; }
+    public int UserId { get; set; }
+    public int FriendId { get; set; }
+
+    public UserLogin User { get; set; }
+    public UserLogin Friend { get; set; }
+}
