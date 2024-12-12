fe16c7fbd6fcc393aa1f1bde60b26300c7cf5ca1
BASTIEN <bastien.grenot@viacesi.fr>
2024-12-12 21:08:02 +0100

    OUI

--- AppLB_Back/API_Csharp/API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ AppLB_Back/API_Csharp/API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7520a5c0168b5ac7dc078dd8ba2f32480bf747ac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+415fbd1f070a8949ffc72aa9c7591ea7fb422a89")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
