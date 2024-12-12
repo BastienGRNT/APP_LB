a4fe0518cbc02a7c9a4e244494c8c1d829ff61e1
BASTIEN <bastien.grenot@viacesi.fr>
2024-12-12 17:18:03 +0100

    q

--- BET APP/API/API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ BET APP/API/API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c7c7323b1d1fe088b7302561b8a6bda24a4ec41")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df177d664c979e66095a55865d7bfd8bc4466802")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
