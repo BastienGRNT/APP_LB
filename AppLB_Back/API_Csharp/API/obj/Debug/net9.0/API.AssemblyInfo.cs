7fb4c4eaa6eebe0b775f5ff9e1da573835d7f9ac
greno <bastien.grenot@gmail.com>
2024-12-27 23:23:36 +0100

    A

--- AppLB_Back/API_Csharp/API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ AppLB_Back/API_Csharp/API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f17e8f3623f8f4b38f37639e87195a8d5d257382")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4a2e092237ab26f3ffaf5455a398e485b0a554b")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
