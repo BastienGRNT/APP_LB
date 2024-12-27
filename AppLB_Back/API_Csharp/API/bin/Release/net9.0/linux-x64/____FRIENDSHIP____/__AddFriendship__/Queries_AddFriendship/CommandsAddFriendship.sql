f17e8f3623f8f4b38f37639e87195a8d5d257382
BASTIEN <bastien.grenot@viacesi.fr>
2024-12-27 02:38:08 +0100

    A

---
+++ AppLB_Back/API_Csharp/API/bin/Release/net9.0/linux-x64/____FRIENDSHIP____/__AddFriendship__/Queries_AddFriendship/CommandsAddFriendship.sql
@@ -0,0 +1,10 @@
+﻿SELECT COUNT(*)
+FROM friendship
+WHERE user_1 = @user2 AND user_2 = @user1;
+
+UPDATE friendship
+SET statut = 'accepté'
+WHERE user_1 = @user1 AND user_2 = @user2;
+
+INSERT INTO friendship (user_1, user_2, statut, date_friendship)
+VALUES (@user1, @user2, @statut, CURRENT_TIMESTAMP);
