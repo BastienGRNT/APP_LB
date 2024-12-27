2c4e3e269f58770339e439b95ce105fea21b73e3
BASTIEN <bastien.grenot@viacesi.fr>
2024-12-27 02:37:58 +0100

    API Publiable

--- AppLB_Back/API_Csharp/API/obj/Release/net9.0/linux-x64/API.AssemblyInfo.cs
+++ AppLB_Back/API_Csharp/API/obj/Release/net9.0/linux-x64/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99bae4fe242bbb4630b464493f5d46d588702ca1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51bc02a625a99ce5b6b23a5f45c0472665cfa8a9")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
